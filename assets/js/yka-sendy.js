77712ef9da3ade66db8a61202b23149d62c05a31
Jay Vardhan <vardhan.jay@gmail.com>
2023-06-01 10:39:57 +0530

    refactored js

--- assets/js/yka-sendy.js
+++ assets/js/yka-sendy.js
@@ -1,32 +1,31 @@
-const checkElement = async (selector) => {
-  while (document.querySelector(selector) === null) {
-    await new Promise((resolve) => requestAnimationFrame(resolve));
-  }
-  return document.querySelector(selector);
-};
+window.addEventListener("load", (event) => {
+  const formElement = document.querySelector(
+    '[data-behaviour="yka-sendy-form"]'
+  );
 
-checkElement('[data-behaviour="yka-sendy-form"]').then((formElement) => {
-  formElement.addEventListener("submit", async function (e) {
-    e.preventDefault();
+  if (formElement) {
+    formElement.addEventListener("submit", async function (e) {
+      e.preventDefault();
 
-    const formData = new FormData(this);
-    const data = new URLSearchParams(); // to convert multipart-form into x-www-url-encoded
+      const formData = new FormData(this);
+      const data = new URLSearchParams(); // to convert multipart-form into x-www-url-encoded
 
-    for (const pair of formData) {
-      data.append(pair[0], pair[1]);
-    }
+      for (const pair of formData) {
+        data.append(pair[0], pair[1]);
+      }
 
-    let response = await fetch(this.getAttribute("action"), {
-      method: "post",
-      body: data,
+      let response = await fetch(this.getAttribute("action"), {
+        method: "post",
+        body: data,
+      });
+
+      let message = await response.text();
+      const status = document.getElementById("sendy-status");
+      if (message == 1) {
+        status.innerHTML = "You're subscribed to our mailing list. Thank You!";
+      } else {
+        status.innerHTML = message;
+      }
     });
-
-    let message = await response.text();
-    const status = document.getElementById("sendy-status");
-    if (message == 1) {
-      status.innerHTML = "You're subscribed to our mailing list. Thank You!";
-    } else {
-      status.innerHTML = message;
-    }
-  });
+  }
 });
